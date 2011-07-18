2160e01d6e7fb14a971c61df2c04aec8de8a76fc
Daniel Meißner <daniel@3st.mine.nu>
2011-07-18 21:50:31 +0200

    Added license.

--- encodaem/encodaem.rb
+++ encodaem/encodaem.rb
@@ -1,3 +1,23 @@
+# encoding: UTF-8
+=begin
+Copyright Daniel Meißner <dm@3st.be>, 2011
+
+This file is part of a Encodaem script for video handling.
+
+This script is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This Script is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Encodaem.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
 require 'rubygems'
 require 'daemons'
 
